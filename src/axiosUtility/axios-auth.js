31dac19e774901eacbf92719289bafce48ea3074
berkaysenkoylu <senkoylu_berkay@yahoo.com>
2020-08-16 12:04:59 +0300

    getting ready to deploy

--- src/axiosUtility/axios-auth.js
+++ src/axiosUtility/axios-auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8000/api/auth'
+    baseURL: 'https://glacial-garden-54072.herokuapp.com/api/auth'
 });
 
 export default instance;
--- src/containers/CourseList/CourseList.js
+++ src/containers/CourseList/CourseList.js
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from 'react';
 
 import classes from './CourseList.module.scss';
+import Loader from '../../components/Loader/Loader';
 import axiosCourse from '../../axiosUtility/axios-course';
 import CourseListElement from './CourseListElement/CourseListElement';
 
 const CourseList = (props) => {
     const [courseList, setCourseList] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
+        setIsLoading(true);
         axiosCourse.get('').then(response => {
             setCourseList(response.data.courseList);
+            setIsLoading(false);
         })
     }, []);
 
@@ -17,22 +21,36 @@ const CourseList = (props) => {
         props.history.push(props.match.url + '/' + id)
     }
 
+    let pageContent = (
+        <span className={classes.CourseList__Loader}>
+            <Loader strokeWidth={4} />
+        </span>
+    );
+
+    if (!isLoading) {
+        pageContent = (
+            <>
+                <header className={classes.CourseList__Header}>
+                    <h1>OUR COURSES</h1>
+                </header>
+            
+                <div className={classes.CourseList__Content}>
+                    { courseList.map((course, i) => {
+                        return <CourseListElement
+                            key={course._id}
+                            data={course}
+                            index={i}
+                            selectCourseListElement={() => onCourseClickedHandler(course._id)}
+                        />;
+                    }) }
+                </div>
+            </>
+        );
+    }
+
     return (
         <section className={classes.CourseList}>
-            <header className={classes.CourseList__Header}>
-                <h1>OUR COURSES</h1>
-            </header>
-        
-            <div className={classes.CourseList__Content}>
-                { courseList.map((course, i) => {
-                    return <CourseListElement
-                        key={course._id}
-                        data={course}
-                        index={i}
-                        selectCourseListElement={() => onCourseClickedHandler(course._id)}
-                    />;
-                }) }
-            </div>
+            {pageContent}
         </section>
     )
 }
