e27d61029ef64a126b3a83ef35a332d227eb828c
berkaysenkoylu <senkoylu_berkay@yahoo.com>
2020-08-16 12:08:54 +0300

    Updates

---
+++ precache-manifest.2883faa1092dc7b7ea5d1b1b97e23d9f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a68a7b50eb5218048c159b6c7a976dca",
+    "url": "/elearning-app/index.html"
+  },
+  {
+    "revision": "df0f16b98fba7cfaa5df",
+    "url": "/elearning-app/static/css/main.f263a3ce.chunk.css"
+  },
+  {
+    "revision": "55a4c8f3b16d1c575e76",
+    "url": "/elearning-app/static/js/2.42ff247b.chunk.js"
+  },
+  {
+    "revision": "4b809c672a0b4653ce4f1e01389eb9b4",
+    "url": "/elearning-app/static/js/2.42ff247b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df0f16b98fba7cfaa5df",
+    "url": "/elearning-app/static/js/main.511e2af1.chunk.js"
+  },
+  {
+    "revision": "d203d574cc6e26ab4b58",
+    "url": "/elearning-app/static/js/runtime-main.69d2c4b2.js"
+  },
+  {
+    "revision": "303de4cf6bc05e6b3e08c387a8208a7b",
+    "url": "/elearning-app/static/media/spine.303de4cf.jpg"
+  },
+  {
+    "revision": "976e37e5d98a9cd0fdadc866acadf449",
+    "url": "/elearning-app/static/media/sprite.976e37e5.svg"
+  }
+]);
